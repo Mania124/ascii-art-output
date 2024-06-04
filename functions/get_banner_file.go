bcdfcb9ef5b4e8ec9acc6cdacde80dee7222f5db
hshikuku <shikukuhezborn@gmail.com>
2024-06-04 15:23:41 +0300

    feat:functionality for different cli arguments

--- functions/get_banner_file.go
+++ functions/get_banner_file.go
@@ -6,14 +6,29 @@ func FileName() string {
 	if len(os.Args) == 2 {
 		return "standard.txt"
 	}
-	switch os.Args[3] {
-	case "standard":
-		return "standard.txt"
-	case "thinkertoy":
-		return "thinkertoy.txt"
-	case "shadow":
-		return "shadow.txt"
-	default:
-		return "standard.txt"
+	if len(os.Args) == 3 {
+		switch os.Args[2] {
+		case "standard":
+			return "standard.txt"
+		case "thinkertoy":
+			return "thinkertoy.txt"
+		case "shadow":
+			return "shadow.txt"
+		default:
+			return "standard.txt"
+		}
 	}
+	if len(os.Args) == 4 {
+		switch os.Args[3] {
+		case "standard":
+			return "standard.txt"
+		case "thinkertoy":
+			return "thinkertoy.txt"
+		case "shadow":
+			return "shadow.txt"
+		default:
+			return "standard.txt"
+		}
+	}
+	return ""
 }
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	o "ascii-art-output/functions"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
+	var outputFile string
+	var input string
 	if len(os.Args) < 2 || len(os.Args) > 4 {
 		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 		os.Exit(0)
+	}
+	if len(os.Args) == 2 {
+		input = os.Args[1]
+		outputFile = "banner.txt"
 	}
 	if len(os.Args) == 4 {
 		if !(os.Args[3] == "standard" || os.Args[3] == "shadow" || os.Args[3] == "thinkertoy") {
@@ -21,10 +26,21 @@ func main() {
 			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			os.Exit(0)
 		}
+		outputFile = os.Args[1][9:]
+		input = os.Args[2]
 	}
-
-	banner := o.FileName()
-	fmt.Println(banner)
-	art := o.BannerArt(banner)
-	fmt.Println(art)
+	if len(os.Args) == 3 {
+		if !(os.Args[2] == "standard" || os.Args[2] == "shadow" || os.Args[2] == "thinkertoy") {
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
+			os.Exit(0)
+		}
+		input = os.Args[1]
+		outputFile = "banner.txt"
+	}
+	fmt.Println(input)
+	fmt.Println(outputFile)
+	//banner := o.FileName()
+	//fmt.Println(banner)
+	//art := o.BannerArt(banner)
+	//fmt.Println(art)
 }
