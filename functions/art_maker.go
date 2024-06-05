bb36397ffed57ef72c74e5cfaf345eb880a1199a
hshikuku <shikukuhezborn@gmail.com>
2024-06-05 16:38:48 +0300

    feat:fine tuning literal newline functionality

--- functions/art_maker.go
+++ functions/art_maker.go
@@ -8,15 +8,23 @@ import (
 
 func Art(s string, mYmap map[int][]string) string {
 	var str string
+	s = strings.ReplaceAll(s, "\n", "\\n")
 	input := strings.Split(s, "\\n")
+	fmt.Println(input)
 	for j, word := range input {
 		if word == "" && j != len(input)-1 {
 			str += fmt.Sprintln()
+			continue
 		}
+
 		for i := 1; i <= 8; i++ {
 			for _, cha := range word {
-				if ok := (cha >= ' ' && cha <= rune(126)); ok {
+				if ok := (cha >= ' ' && cha <= rune(126)) || (cha == '\n'); ok {
+					// if cha == '\n' {
+					// 	str += "\n"
+					// } else {
 					str += mYmap[int(cha)][i]
+					//}
 				} else if !ok {
 					fmt.Println("Unprintable character within input")
 					os.Exit(1)
@@ -24,7 +32,7 @@ func Art(s string, mYmap map[int][]string) string {
 			}
 			str += fmt.Sprintln()
 		}
-
 	}
+
 	return str
 }
