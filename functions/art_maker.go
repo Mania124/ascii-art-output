9e8719dc54155f9df78c7f9afd5975fba7a1d923
hshikuku <shikukuhezborn@gmail.com>
2024-06-10 10:19:38 +0300

    feat:newline only input

--- functions/art_maker.go
+++ functions/art_maker.go
@@ -10,27 +10,22 @@ func Art(s string, mYmap map[int][]string) string {
 	var str string
 	s = strings.ReplaceAll(s, "\n", "\\n")
 	input := strings.Split(s, "\\n")
-	fmt.Println(input)
 	for j, word := range input {
 		if word == "" && j != len(input)-1 {
 			str += fmt.Sprintln()
 			continue
-		}
-
-		for i := 1; i <= 8; i++ {
-			for _, cha := range word {
-				if ok := (cha >= ' ' && cha <= rune(126)) || (cha == '\n'); ok {
-					// if cha == '\n' {
-					// 	str += "\n"
-					// } else {
-					str += mYmap[int(cha)][i]
-					//}
-				} else if !ok {
-					fmt.Println("Unprintable character within input")
-					os.Exit(1)
+		} else if word != "" {
+			for i := 1; i <= 8; i++ {
+				for _, cha := range word {
+					if ok := (cha >= ' ' && cha <= rune(126)) || (cha == '\n') && word != ""; ok {
+						str += mYmap[int(cha)][i]
+					} else if !ok {
+						fmt.Println("Unprintable character within input")
+						os.Exit(1)
+					}
 				}
+				str += fmt.Sprintln()
 			}
-			str += fmt.Sprintln()
 		}
 	}
 
