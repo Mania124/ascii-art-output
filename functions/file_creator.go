75e6d971145a081478aaeb661e96cedb3caff401
moseeh <mosesadrian825@gmail.com>
2024-06-10 15:24:08 +0300

    fix: add comments

--- functions/file_creator.go
+++ functions/file_creator.go
@@ -6,12 +6,13 @@ import (
 )
 
 func CreateFile(FilePath string, content string) error {
+	// creating a file
 	file, err := os.Create(FilePath)
 	if err != nil {
 		return fmt.Errorf("error in file creation: %w", err)
 	}
 	defer file.Close()
-
+	// writing into the file created
 	_, err = file.WriteString(content)
 	if err != nil {
 		return fmt.Errorf("error in writing content: %w", err)
