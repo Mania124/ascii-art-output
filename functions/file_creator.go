c37b1270c63a55d3d4dcbc3fe613a83c101dd81a
hshikuku <shikukuhezborn@gmail.com>
2024-06-11 08:13:25 +0300

    fix:correct commenting in file creator

--- functions/file_creator.go
+++ functions/file_creator.go
@@ -5,14 +5,15 @@ import (
 	"os"
 )
 
+//This function takes a filepath and  variable holding a string literal as arguments
+//It then creates the file using os package(with respect to the filepath) and writes the string into the file
+//Upon encountering any problems in execution of th two operations, it returns an error
 func CreateFile(FilePath string, content string) error {
-	// creating a file
 	file, err := os.Create(FilePath)
 	if err != nil {
 		return fmt.Errorf("error in file creation: %w", err)
 	}
 	defer file.Close()
-	// writing into the file created
 	_, err = file.WriteString(content)
 	if err != nil {
 		return fmt.Errorf("error in writing content: %w", err)
