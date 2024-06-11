3aa95fc95d474efcad6b7914181eb4a6b778e670
beopiyo <benardopiyo13@gmail.com>
2024-06-11 12:51:08 +0300

    fix: remove comments from get_banner_file_test.go

--- functions/get_banner_file_test.go
+++ functions/get_banner_file_test.go
@@ -11,23 +11,23 @@ func TestFileName(t *testing.T) {
 		args     []string
 		expected string
 	}{
-		{"NoArgs", []string{"progName"}, ""}, // No CLI args
-		{"TwoArgs", []string{"progName", "arg1"}, "standard.txt"}, // Two CLI args
-		{"ThreeArgs", []string{"progName", "arg1", "arg2"}, "standard.txt"}, // Three CLI args
-		{"FourArgsStandard", []string{"progName", "arg1", "arg2", "standard"}, "standard.txt"}, // Four args, style "standard"
-		{"FourArgsThinkertoy", []string{"progName", "arg1", "arg2", "thinkertoy"}, "thinkertoy.txt"}, // Four args, style "thinkertoy"
-		{"FourArgsShadow", []string{"progName", "arg1", "arg2", "shadow"}, "shadow.txt"}, // Four args, style "shadow"
-		{"FourArgsInvalid", []string{"progName", "arg1", "arg2", "invalid"}, "standard.txt"}, // Four args, invalid style
+		{"NoArgs", []string{"progName"}, ""},
+		{"TwoArgs", []string{"progName", "arg1"}, "standard.txt"},
+		{"ThreeArgs", []string{"progName", "arg1", "arg2"}, "standard.txt"},
+		{"FourArgsStandard", []string{"progName", "arg1", "arg2", "standard"}, "standard.txt"},
+		{"FourArgsThinkertoy", []string{"progName", "arg1", "arg2", "thinkertoy"}, "thinkertoy.txt"},
+		{"FourArgsShadow", []string{"progName", "arg1", "arg2", "shadow"}, "shadow.txt"},
+		{"FourArgsInvalid", []string{"progName", "arg1", "arg2", "invalid"}, "standard.txt"},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			originalArgs := os.Args            // Save the original os.Args
+			originalArgs := os.Args                   // Save the original os.Args
 			defer func() { os.Args = originalArgs }() // Restore os.Args after the test
-			os.Args = tc.args                  // Set os.Args to the test case arguments
+			os.Args = tc.args                         // Set os.Args to the test case arguments
 
-			if got := FileName(); got != tc.expected { 
-				t.Errorf("FileName() = %v, want %v", got, tc.expected) 
+			if got := FileName(); got != tc.expected {
+				t.Errorf("FileName() = %v, want %v", got, tc.expected)
 			}
 		})
 	}
